99b42e8c2baede6cecdb01bf525cb8c2a36dc279
Sarah Elzayat <sarahelzayat@outlook.com>
2021-12-24 21:27:17 +0200

    Employees gui

---
+++ TrainStation/CancelTicket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrainStation
+{
+    public partial class CancelTicket : Form
+    {
+        public CancelTicket()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("You succefully cancelled a ticket with Serial No. : ");
+        }
+    }
+}
