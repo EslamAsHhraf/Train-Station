4c2e11c1c112a1d2f017a6abd453d885ab2cb9d9
EslamAsHhraf <eslamashraf.12342@gmail.com>
2022-01-07 19:02:07 +0200

    Schema

--- TrainStation/Manager_forms/Schedule_trips.cs
+++ TrainStation/Manager_forms/Schedule_trips.cs
@@ -25,12 +25,12 @@ namespace TrainStation.Manager_forms
 
         private void Schedule_trips_Load(object sender, EventArgs e)
         {
-            for(int i=0;i<60;i++)
+            for (int i = 0; i < 60; i++)
             {
                 MA.Items.Add(i.ToString());
                 MD.Items.Add(i.ToString());
-            } 
-            for(int i=8;i<20;i++)
+            }
+            for (int i = 8; i < 20; i++)
             {
                 HA.Items.Add(i.ToString());
                 HD.Items.Add(i.ToString());
@@ -41,7 +41,7 @@ namespace TrainStation.Manager_forms
             DataTable dt2 = con.ID_Station();
             comestation.DataSource = dt2;
             comestation.DisplayMember = "ID";
-            DataTable dt3= con.ID_Station();
+            DataTable dt3 = con.ID_Station();
             Gostation.DataSource = dt3;
             Gostation.DisplayMember = "ID";
         }
@@ -49,7 +49,7 @@ namespace TrainStation.Manager_forms
         private void Insert_trip_Click(object sender, EventArgs e)
         {
             Trip_code = hand.Trim(Trip_code);
-            if (Trip_code.Text.Length==0)
+            if (Trip_code.Text.Length == 0)
             {
                 MessageBox.Show("Please Insert Trip Code!");
                 return;
@@ -61,7 +61,7 @@ namespace TrainStation.Manager_forms
                 MessageBox.Show("you can't insert ;");
                 return;
             }
-            if(VIP.Text.Length==0)
+            if (VIP.Text.Length == 0)
             {
                 MessageBox.Show("Please Insert Price of VIP!");
                 return;
@@ -81,17 +81,17 @@ namespace TrainStation.Manager_forms
                 MessageBox.Show("Please Insert Number of General Tickets!");
                 return;
             }
-            if (HA.SelectedIndex==-1|| MA.SelectedIndex == -1)
+            if (HA.SelectedIndex == -1 || MA.SelectedIndex == -1)
             {
                 MessageBox.Show("Please Insert Arrival Time!");
                 return;
             }
-            if (HD.SelectedIndex == -1 || MD.SelectedIndex == -1 )
+            if (HD.SelectedIndex == -1 || MD.SelectedIndex == -1)
             {
                 MessageBox.Show("Please Insert Departure Time!");
                 return;
             }
-            if(VIP.Text.Length>=5 || GENERAL.Text.Length >=5)
+            if (VIP.Text.Length >= 5 || GENERAL.Text.Length >= 5)
             {
                 MessageBox.Show("Price of Vip and General should be less than 5 digits!");
                 return;
@@ -101,50 +101,50 @@ namespace TrainStation.Manager_forms
                 MessageBox.Show("Number of Vip and General tickets should be less than 5 digits!");
                 return;
             }
-            string arrivel = HA.Text + ':' + MA.Text ;
-            string departure= HD.Text + ':' + MD.Text ;
+            string arrivel = HA.Text + ':' + MA.Text;
+            string departure = HD.Text + ':' + MD.Text;
             String r1 = con.Is_Trip(Trip_code.Text);
-            if(r1!=null)
+            if (r1 != null)
             {
                 MessageBox.Show("This Trip is already exists!");
                 return;
             }
-            int h1,h2,m1,m2;
+            int h1, h2, m1, m2;
             Int32.TryParse(HA.Text, out h1);
             Int32.TryParse(HD.Text, out h2);
             Int32.TryParse(MA.Text, out m1);
             Int32.TryParse(MD.Text, out m2);
             bool check = false;
-            if(h1>h2)
+            if (h1 > h2)
             {
                 check = true;
             }
-            else if(h1==h2)
+            else if (h1 == h2)
             {
                 if (m1 > m2)
                 {
                     check = true;
                 }
             }
-            if(check)
+            if (check)
             {
                 MessageBox.Show("Departure Time should be longer than Arrival Time!");
                 return;
             }
-            int r2 = con.Insert_Trip(Trip_code.Text, arrivel, departure,trainNum.Text,comestation.Text,Gostation.Text);
-            if(r2==0)
-            { 
+            int r2 = con.Insert_Trip(Trip_code.Text, arrivel, departure, trainNum.Text, comestation.Text, Gostation.Text);
+            if (r2 == 0)
+            {
                 MessageBox.Show("Trip insert failed!");
             }
             else
-            {  
+            {
                 MessageBox.Show("Trip inserted successfully");
                 /*for vip*/
                 int vnum, gnum;
                 Int32.TryParse(Num_VIP.Text, out vnum);
                 Int32.TryParse(NUM_G.Text, out gnum);
-                int num_ticke=0;
-                for(int i=0;i< vnum; i++)
+                int num_ticke = 0;
+                for (int i = 0; i < vnum; i++)
                 {
                     string temp;
                     int num;
@@ -155,7 +155,7 @@ namespace TrainStation.Manager_forms
                         int ti = con.Is_ticket(temp);
                         if (ti == 0)
                         {
-                            num_ticke+= con.Insetr_ticket(temp,"V",VIP.Text, Date_Of_trip.Text, Trip_code.Text);
+                            num_ticke += con.Insetr_ticket(temp, "V", VIP.Text, Date_Of_trip.Text, Trip_code.Text);
                             break;
                         }
                     }
@@ -177,16 +177,8 @@ namespace TrainStation.Manager_forms
                         }
                     }
                 }
-                if(-1*num_ticke == gnum+ vnum)
-                {
-                    MessageBox.Show("Ticket inserted successfully");
-                }
-                else
-                {
-                    MessageBox.Show("Ticket inserted failed!");
-                }
             }
-}
+        } 
 
         private void Exit_Click(object sender, EventArgs e)
         {
